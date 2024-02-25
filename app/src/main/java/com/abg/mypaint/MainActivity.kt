4640f43eb937f0e03e4ced698f21bcac4f2f3d54
vadim <gabderahmanov99@gmail.com>
2024-02-25 12:22:31 +0300

    open image

--- app/src/main/java/com/abg/mypaint/MainActivity.kt
+++ app/src/main/java/com/abg/mypaint/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.abg.mypaint
 
 import android.Manifest
+import android.content.Intent
 import android.graphics.Bitmap
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.abg.mypaint.local.ManagerFile
 import com.abg.mypaint.ui.DrawableFragment
@@ -17,11 +19,15 @@ import com.fondesa.kpermissions.request.PermissionRequest
 
 class MainActivity : AppCompatActivity(), DrawableFragment.FileHandler, PermissionRequest.Listener {
 
+    companion object {
+        private const val REQUEST_CODE_OPEN_DIRECTORY = 13433
+    }
+
     private val request by lazy {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
             permissionsBuilder(Manifest.permission.READ_MEDIA_IMAGES).build()
         } else {
-            permissionsBuilder(Manifest.permission.WRITE_EXTERNAL_STORAGE).build()
+            permissionsBuilder(Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE).build()
         }
     }
 
@@ -29,7 +35,8 @@ class MainActivity : AppCompatActivity(), DrawableFragment.FileHandler, Permissi
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
             isPermissionGranted(Manifest.permission.READ_MEDIA_IMAGES)
         } else {
-            isPermissionGranted(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            isPermissionGranted(Manifest.permission.WRITE_EXTERNAL_STORAGE) &&
+                    isPermissionGranted(Manifest.permission.READ_EXTERNAL_STORAGE)
         }
     }
 
@@ -52,6 +59,12 @@ class MainActivity : AppCompatActivity(), DrawableFragment.FileHandler, Permissi
         }
     }
 
+    override fun openImage() {
+        val intent = Intent(Intent.ACTION_OPEN_DOCUMENT)
+        intent.setType("application/pdf")
+        startActivityForResult(intent, REQUEST_CODE_OPEN_DIRECTORY)
+    }
+
     override fun onPermissionsResult(result: List<PermissionStatus>) {
         when {
             result.anyPermanentlyDenied() -> showPermanentlyDeniedDialog(result)
@@ -59,6 +72,17 @@ class MainActivity : AppCompatActivity(), DrawableFragment.FileHandler, Permissi
             result.allGranted() -> {
                 showGrantedToast(result)
             }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == REQUEST_CODE_OPEN_DIRECTORY && resultCode == AppCompatActivity.RESULT_OK) {
+            Log.d("ddd", String.format("Open Directory result Uri : %s", data!!.data))
+            val args = Bundle()
+            args.putString("uri", data.data.toString())
+
         }
     }
 }
